f701546e0c18d973d64f66c928f4a4468ffd867e
matsuda <kosukematsuda@gmail.com>
2021-01-03 17:10:24 +0900

    create project

---
+++ FastlaneSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FastlaneSample
+//
+//  Created by Kosuke Matsuda on 2021/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
